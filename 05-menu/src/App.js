41aa3534e459a125479bc1386d28b55f960dd5a0
ClaireKarsenti <102292921+ClaireKarsenti@users.noreply.github.com>
2022-07-13 21:11:03 +0200

    menu project

---
+++ 05-menu/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+/* I use new Set() because I only want the name of the category one time. 
+So if a category is repeat four times, then his name will be a unique name.
+Also, if a new category is added to the data files a new button with the category's name will be display as well.*/
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section"></section>
+      <div className="title">
+        <h2>our menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems} />
+    </main>
+  );
+}
+
+export default App;
