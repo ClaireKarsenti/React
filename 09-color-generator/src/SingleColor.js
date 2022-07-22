f32467cf05935246642e48650d210d828bae9938
ClaireKarsenti <102292921+ClaireKarsenti@users.noreply.github.com>
2022-07-22 19:10:33 +0200

    Color Generator project

---
+++ 09-color-generator/src/SingleColor.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`} // The text will be black on light color and white on darker one (>10).
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true); // Display the alert "copied to clipboard"
+        navigator.clipboard.writeText(hexValue); // Copy the hexvalue in the clipboard when I click on the color
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
